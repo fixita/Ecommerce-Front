143764a968867feaf63a8e6e7f6cd9950fa535df
fixita <ismaismael357@gmail.com>
2024-10-03 18:11:29 -0300

    feat: :art: Modificacion layout customer

--- src/app/pages/customer/customer-layout/customer-layout.component.ts
+++ src/app/pages/customer/customer-layout/customer-layout.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../../shared/components/product-card/product-card.component';
+import { CategoryCardComponent } from '../../shared/components/category-card/category-card.component';
 
 @Component({
   selector: 'app-customer-layout',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, CommonModule],
+  imports: [HeaderComponent, FooterComponent, CommonModule, ProductCardComponent, CategoryCardComponent],
   templateUrl: './customer-layout.component.html',
   styleUrl: './customer-layout.component.css'
 })
