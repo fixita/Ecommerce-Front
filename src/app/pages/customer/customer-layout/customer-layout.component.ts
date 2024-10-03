7dfb862df889834fff2dbc8c7909da235e9ca9a6
SebaVRdev <sebarm177@gmail.com>
2024-10-03 20:14:31 -0300

    fix: La carpeta shared y componentes reutilizables quedan fuera de pages

--- src/app/pages/customer/customer-layout/customer-layout.component.ts
+++ src/app/pages/customer/customer-layout/customer-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../shared/components/header/header.component";
-import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';
-import { ProductCardComponent } from '../../shared/components/product-card/product-card.component';
-import { CategoryCardComponent } from '../../shared/components/category-card/category-card.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { ProductCardComponent } from '../../../shared/components/product-card/product-card.component';
+import { CategoryCardComponent } from '../../../shared/components/category-card/category-card.component';
+
 
 @Component({
   selector: 'app-customer-layout',
