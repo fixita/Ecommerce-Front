557d8ddc3777ada703709a64c4b7146b5b67bd9d
fixita <ismaismael357@gmail.com>
2024-10-05 03:17:06 -0300

    feat: :zap: Se agrega informacion de la empresa

---
+++ src/app/shared/components/company-information/company-information.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-information',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-information.component.html',
+  styleUrl: './company-information.component.css'
+})
+export class CompanyInformationComponent {
+
+}
